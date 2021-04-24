5e2685dcf162ac7904323d367b990e96aa4aaea0
Sean Callan <sean@system76.com>
2021-04-24 08:27:15 -0600

    Add CORSPlug to handle cross domain requests

--- lib/tiny_web/endpoint.ex
+++ lib/tiny_web/endpoint.ex
@@ -39,6 +39,7 @@ defmodule TinyWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug CORSPlug
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
